48d08515a59e82ba4a048269dfd1faee3d3f32ef
Mike Peterson <mpeterson2004@gmail.com>
2021-05-13 17:08:40 -0600

    admin page is working and song objects now exist

--- music_library/music/models.py
+++ music_library/music/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 class Song(models.Model):
-    title = models.Charfield(max_length=50)
-    artist = models.Charfield(max_length=50)
-    album = models.Charfield(max_length=50)
-    release_date = models.DateTimeField()
-    likes = models.IntegerField(default=None)
+    title = models.CharField(max_length=50)
+    artist = models.CharField(max_length=50)
+    album = models.CharField(max_length=50)
+    release_date = models.DateField()
+    likes = models.IntegerField(default=0)
