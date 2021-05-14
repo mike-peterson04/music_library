42c188fe9187a1a5a138881fd42c6e5e735bc972
Mike Peterson <mpeterson2004@gmail.com>
2021-05-14 11:08:20 -0600

    added path for individual song based on PK and individual song get hook

--- music_library/music/urls.py
+++ music_library/music/urls.py
@@ -19,4 +19,5 @@ from . import views
 
 urlpatterns = [
     path('music/', views.SongList.as_view()),
+    path('music/<int:song_id>/', views.SongDetail.as_view())
 ]
--- music_library/music/views.py
+++ music_library/music/views.py
@@ -7,15 +7,28 @@ from rest_framework import status
 
 # Create your views here.
 class SongList(APIView):
+
     def get(self, request):
         songs = Song.objects.all()
         serializer = SongSerializer(songs, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
 
     def post(self, request):
         serializer = SongSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SongDetail(APIView):
+
+    def get(self, request, song_id):
+        try:
+            song = Song.objects.get(pk=song_id)
+            serializer = SongSerializer(song)
+            return Response(serializer.data, status.HTTP_200_OK)
+
+        except:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+ 
