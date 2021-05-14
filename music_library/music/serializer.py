2c63ed50615764e0649828a3f22dd39afb566e49
Mike Peterson <mpeterson2004@gmail.com>
2021-05-14 11:06:05 -0600

    added update method to serializer

--- music_library/music/serializer.py
+++ music_library/music/serializer.py
@@ -5,4 +5,13 @@ from .models import Song
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
-        fields = ['id', 'title', 'album', 'release_date', 'likes']
+        fields = ['id', 'title', 'artist', 'album', 'release_date', 'likes']
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.artist = validated_data.get('artist', instance.artist)
+        instance.album = validated_data.get('album', instance.album)
+        instance.release_date = validated_data.get('release_date', instance.release_date)
+        instance.likes = validated_data.get('likes', instance.likes)
+        instance.save()
+        return instance
