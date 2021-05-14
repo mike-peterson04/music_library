d168e4ceffb871843880378219a9c24167c92b26
Mike Peterson <mpeterson2004@gmail.com>
2021-05-13 18:29:01 -0600

    SongList api hook is working for get/post

--- music_library/music/urls.py
+++ music_library/music/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('music.urls'))
+    path('music/', views.SongList.as_view()),
 ]
--- music_library/music/views.py
+++ music_library/music/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Song
+from .serializer import SongSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+class SongList(APIView):
+    def get(self, request):
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    def post(self, request):
+        serializer = SongSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
