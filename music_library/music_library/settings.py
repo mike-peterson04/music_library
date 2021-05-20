2485c57311648eebb4778eeca653083dcb418202
Mike Peterson <mpeterson2004@gmail.com>
2021-05-20 13:38:28 -0600

    added cors

--- music_library/music_library/settings.py
+++ music_library/music_library/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'meo0av$vp^=lz8t26u(wf_2y99u%az7v&h0qvh^248hh)(67ag'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*'
+]
 
 
 # Application definition
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,10 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGNS = [
+    "http://localhost:3000/"
+]
 
 ROOT_URLCONF = 'music_library.urls'
 
